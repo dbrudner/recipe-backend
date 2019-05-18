6e8efed19d312b433eefc1260b07f4232fd14f97
unknown <dbrudne1@gmail.com>
2019-05-18 11:05:32 -0500

    w mongo

--- server.js
+++ server.js
@@ -2,19 +2,31 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 3000;
-const db = require("./models");
 
-mongoose.connection.on("open", function() {
-	console.log("Connected to mongo server.");
-});
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+MongoClient.connect(
+	process.env.MONGO_URI,
+	{ useNewUrlParser: true },
+	(err, client) => {
+		if (err) {
+			throw err;
+		}
+	},
+);
 
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(cors());
+
+server.get("/hey", (req, res) => {
+	res.send("Hey");
+});
+
+server.listen(port, err => {
+	if (err) throw err;
+	console.log(`> Ready on http://localhost:${port}`);
+});
