0d4133d1e6040567d1a60246ce4b18a8cd70e171
Dave Brudner <dbrudne1@gmail.com>
2019-05-19 17:04:00 -0500

    eslint

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-const db = require("./models");
 
 mongoose.connection.on("open", function() {
+	// eslint-disable-next-line
 	console.log("Connected to mongo server.");
 });
 
@@ -22,5 +21,7 @@ app.use("/", require("./api"));
 
 app.listen(port, err => {
 	if (err) throw err;
+
+	// eslint-disable-next-line
 	console.log(`> Ready on http://localhost:${port}`);
 });
